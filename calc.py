6f610b0d38d5ef13df04e606268fd6132ca34ef6
jnejati <j.nejati@gmail.com>
2020-06-21 15:06:55 -0400

    Divide Function

--- calc.py
+++ calc.py
@@ -3,3 +3,8 @@ form math import pi
 #Calculates a cirecle's area
 def calct_area(r):
     return pi * r **2
+
+def calc_divide(a, b):
+    if b == 0:
+	raise ValueError('Division by Zero)
+    return a/b
